af263bac9b8fde0a0c4a8a8bc10d9a169f25832d
Sunnatbek077 <sunnatbekabdunabiyev@icloud.com>
2025-03-26 14:23:19 +0500

    Added readme and gitignore

--- RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
+++ RickAndMorty/Controllers/Core/RMEpisodeViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-/// Controller to show and search for Episodes 
+/// Controller to show and search for Episodes
 final class RMEpisodeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
